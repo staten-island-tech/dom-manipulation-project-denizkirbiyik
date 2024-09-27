fd73ba8c9478c1091725cb2fc1b4a76c5c1826a0
Deniz Kirbiyik <58996968+denizkirbiyik@users.noreply.github.com>
2024-09-27 12:22:17 -0400

    m

--- index.js
+++ index.js
@@ -2,17 +2,28 @@ function clearInput() {
   document.getElementById("prompt").value = "";
 }
 
+function removePrompt(num) {
+  const parent = document.getElementById("parent");
+  parent.removeChild(`p${num}`);
+}
+
 function addElement(prompt) {
-  const parent = document.getElementById("container");
+  const parent = document.getElementById("parent");
   const child = document.createElement("div");
   child.classList.add("child");
-  child.textContent = prompt;
+  child.id = `p${parent.childElementCount}`;
+  child.textContent = `${prompt} `;
+  const remove = document.createElement("BUTTON");
+  remove.textContent = "Remove";
+  remove.onclick = function () {
+    parent.removeChild(child);
+  };
+  child.append(remove);
   parent.appendChild(child);
 }
 
 function onClick() {
   const prompt = document.getElementById("prompt").value;
-  console.log(prompt);
   clearInput();
   addElement(prompt);
 }
